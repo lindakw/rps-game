a2973beae2f93623b70bd23d59c343a662b4b07f
lindakw <linkda01@icloud.com>
2021-12-02 12:00:21 -0800

    database connect/styling

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, Pressable } from "react-native";
+import { getDatabase, ref, onValue, set } from "firebase/database";
+import "./firebase";
 
 export default function App() {
   const [highScore, setHighScore] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
-  const gameChoices = ['rock', 'paper', 'scissors'];
-  const [outCome, setOutCome] = useState({user: "", computer: ""});
+  const gameChoices = ["rock", "paper", "scissors"];
+  const [outCome, setOutCome] = useState({ user: "", computer: "" });
+  const db = getDatabase();
+  const reference = ref(db, "highscore/");
+
+  useEffect(() => {
+    onValue(reference, (snapshot) => {
+      if (snapshot.val() !== null) {
+        const highscore = snapshot.val().highscore;
+        setHighScore(highscore);
+      }
+    });
+  }, []);
+
+  const storeHighScore = (score) => {
+    set(reference, {
+      highscore: score,
+    });
+  };
 
   const playRound = (userChoice) => {
     const computerChoice = Math.floor(Math.random() * 3);
-    setOutCome({user: userChoice, computer: gameChoices[computerChoice]});
-    
+    setOutCome({ user: userChoice, computer: gameChoices[computerChoice] });
+
     if (userChoice === gameChoices[computerChoice]) return;
 
     if (
@@ -22,23 +41,33 @@ export default function App() {
       setCurrentScore(currentScore + 1);
       if (currentScore >= highScore) {
         setHighScore(currentScore + 1);
-        // storeHighScore(currentScore + 1);
+        storeHighScore(currentScore + 1);
       }
     } else {
       setCurrentScore(0);
     }
-  }
-
-
-
+  };
 
   return (
     <View style={styles.container}>
-      <Text>{highScore}</Text>
-      <Button onPress={() => playRound("rock")} title="Rock"/>
-      <Button onPress={() => playRound("paper")} title="Paper"/>
-      <Button onPress={() => playRound("scissors")} title="Scissors"/>
-      <Text>{currentScore}</Text>
+      <Text style={styles.score}>{highScore}</Text>
+      <View style={styles.btnContainer}>
+        <Pressable
+          style={styles.btn}
+          onPress={() => playRound("rock")}
+          title="Rock"
+        >
+          <Text>Rock</Text>
+        </Pressable>
+        <Pressable style={styles.btn} onPress={() => playRound("paper")}>
+          <Text>Paper</Text>
+        </Pressable>
+        <Pressable style={styles.btn} onPress={() => playRound("scissors")}>
+          <Text>Scissors</Text>
+        </Pressable>
+      </View>
+      <Text style={styles.score}>{currentScore}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -47,8 +76,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  btn: {
+    margin: 10,
+    backgroundColor: "steelblue",
+    padding: 10,
+    width: 75,
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  score: {
+    fontSize: 32,
+    fontWeight: "bold",
+    margin: 40,
   },
 });
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArbf195CjxstDejerIpQ7QgWkyanolBfg",
+  authDomain: "rps-game-aa925.firebaseapp.com",
+  projectId: "rps-game-aa925",
+  storageBucket: "rps-game-aa925.appspot.com",
+  messagingSenderId: "418536473389",
+  appId: "1:418536473389:web:5458aafba4083e500349a4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
